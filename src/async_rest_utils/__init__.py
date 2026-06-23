95a95c124993431e52f0b3839e29d316b2d06151
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2026-06-23 09:47:53 -0500

    Add inject_cfg_value to module exports

--- src/async_rest_utils/__init__.py
+++ src/async_rest_utils/__init__.py
@@ -1,10 +1,12 @@
-from async_rest_utils.config import load_rest_configs
-from async_rest_utils.exceptions import HttpResponseError
-from async_rest_utils.http import parse_response_body, send_request
-from async_rest_utils.secrets import fetch_secret
-from async_rest_utils.sink import sink_rows_to_file
+from .mutation import inject_cfg_value
+from .config import load_rest_configs
+from .exceptions import HttpResponseError
+from .http import parse_response_body, send_request
+from .secrets import fetch_secret
+from .sink import sink_rows_to_file
 
 __all__ = [
+    "inject_cfg_value",
     "HttpResponseError",
     "fetch_secret",
     "load_rest_configs",
