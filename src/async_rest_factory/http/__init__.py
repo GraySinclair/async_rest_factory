e824914a01491f77db8fb18163639bd6500bd94d
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2026-07-09 15:21:30 -0500

    Update import paths in __init__.py
    fixed import statement

--- src/async_rest_factory/http/__init__.py
+++ src/async_rest_factory/http/__init__.py
@@ -1,5 +1,5 @@
-from async_rest_utils.http.client import send_request
-from async_rest_utils.http.parsing import parse_response_body
+from async_rest_factory.http.client import send_request
+from async_rest_factory.http.parsing import parse_response_body
 
 __all__ = [
     "send_request",
